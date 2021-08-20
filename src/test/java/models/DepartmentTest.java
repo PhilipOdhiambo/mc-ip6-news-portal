7ebe36d131749ffd2a65fca09652f2b025a39c0e
Philip Odhiambo <jofonowino@gmail.com>
2021-08-20 17:16:47 +0300

    create passing test for new Department instance

---
+++ src/test/java/models/DepartmentTest.java
@@ -0,0 +1,18 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentTest {
+    @Test
+    public void departmentInstance_newDepartmentInstanceCreated_department() {
+        Department department = getNewDepartment();
+        assertTrue(department instanceof Department);
+    }
+
+    // helpers
+    public Department getNewDepartment() {
+        return  new Department("Administration");
+    }
+}
